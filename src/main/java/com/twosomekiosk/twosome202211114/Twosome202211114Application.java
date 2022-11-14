baf8b1a4a0d41011f02fe1630f46b652485a1ae6
Lee Jugeon0129 <4216ljg@naver.com>
2022-11-14 17:06:56 +0900

    first commit

---
+++ src/main/java/com/twosomekiosk/twosome202211114/Twosome202211114Application.java
@@ -0,0 +1,13 @@
+package com.twosomekiosk.twosome202211114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twosome202211114Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Twosome202211114Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/twosomekiosk/twosome202211114/Twosome202211114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twosomekiosk.twosome202211114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Twosome202211114ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
