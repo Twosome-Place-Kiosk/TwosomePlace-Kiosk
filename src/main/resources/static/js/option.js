70d32d362518e5f1e7354c5ffbd45d1b2751af67
Lee Jugeon0129 <4216ljg@naver.com>
2022-11-17 18:03:17 +0900

    옵션 선택 checkbox 갯수 제한 작업중

--- src/main/resources/static/js/option.js
+++ src/main/resources/static/js/option.js
@@ -1,27 +1,27 @@
-const cups = document.querySelectorAll(".cup");
-for(const cup of cups) {
-    cup.addEventListener('click', function(event){
-        cup.style.border="3px solid red";
-    })
-}
+// const cups = document.querySelectorAll(".cup");
+// for(const cup of cups) {
+//     cup.addEventListener('click', function(event){
+//         cup.style.border="3px solid red";
+//     })
+// }
 
-const ices = document.querySelectorAll(".ice");
-for(const ice of ices) {
-    ice.addEventListener('click', function(event){
-        ice.style.border="3px solid red";
-    })
-}
+// const ices = document.querySelectorAll(".ice");
+// for(const ice of ices) {
+//     ice.addEventListener('click', function(event){
+//         ice.style.border="3px solid red";
+//     })
+// }
 
-const sugers = document.querySelectorAll(".suger");
-for(const suger of sugers) {
-    suger.addEventListener('click', function(event){
-        suger.style.border="3px solid red";
-    })
-}
+// const sugers = document.querySelectorAll(".suger");
+// for(const suger of sugers) {
+//     suger.addEventListener('click', function(event){
+//         suger.style.border="3px solid red";
+//     })
+// }
 
-const selects = document.querySelectorAll(".topping");
-for(const select of selects) {
-    select.addEventListener('click', function(event){
-        select.style.border="3px solid red";
-    })
-}
+// const selects = document.querySelectorAll(".topping");
+// for(const select of selects) {
+//     select.addEventListener('click', function(event){
+//         select.style.border="3px solid red";
+//     })
+// }
