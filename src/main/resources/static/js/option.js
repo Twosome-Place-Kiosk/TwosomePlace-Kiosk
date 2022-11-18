f9ddc3f85eb029335496a9687ece311d749e87cd
Lee Jugeon0129 <4216ljg@naver.com>
2022-11-18 16:54:48 +0900

    옵션 페이지 체크박스 갯수 제한 완료, 추가옵션 슬라이드 작업 필요

--- src/main/resources/static/js/option.js
+++ src/main/resources/static/js/option.js
@@ -25,3 +25,35 @@
 //         select.style.border="3px solid red";
 //     })
 // }
+// const toppingInputs = document.querySelectorAll(".modal-toppingbox input[type=checkbox]");
+// let count = 0;
+
+// for(const toppingInput of toppingInputs){
+//     if(count < 3) {
+//         toppingInput.addEventListener('click', countCheck);
+//         count++;
+//         console.log(count);
+//     }
+// } 
+
+
+function countCheck(obj){
+    const toppingLabels = document.querySelectorAll(".modal-toppingbox input[type=checkbox]");
+    let checkCount = 0;
+
+    for(let i = 0; i< toppingLabels.length; i++) {
+        if(toppingLabels[i].checked){
+            checkCount++;
+        }
+    }
+    
+    // console.log(checkCount);
+    if(checkCount > 3){
+        alert("3개까지 체크할 수 있습니다.");
+        obj.checked = false;
+        return false;
+    }
+}
+
+
+
