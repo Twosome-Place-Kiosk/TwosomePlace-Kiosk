3bedb282032805ce73d5cfc28ebc298b45824eb6
YoungWoong22 <hwa8078678@naver.com>
2022-11-22 15:43:27 +0900

    로그인시 admin/product 페이지로 이동 성공!!

--- src/main/java/com/twosomekiosk/twosome202211114/controller/admin/AdminProduct.java
+++ src/main/java/com/twosomekiosk/twosome202211114/controller/admin/AdminProduct.java
@@ -2,11 +2,12 @@ package com.twosomekiosk.twosome202211114.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminProduct {
-    @GetMapping("/product")
+    @PostMapping("/product")
     public String login() {return "admin/product_registration";}
 }
