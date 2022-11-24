1a74c3bbc9b71fc45d0dc4cac58289285c749b2d
Lee Jugeon0129 <4216ljg@naver.com>
2022-11-24 21:36:05 +0900

    톱니바퀴 작업중

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,47 @@
+const products = document.querySelectorAll(".product");
+
+for(const product of products) {
+    product.addEventListener('click', function(event){
+        
+        var modalPop = $('.modal-wrap');
+        var modalBg = $('.modal-bg');
+
+        $(modalPop).show();
+        $(modalBg).show();
+    })
+}
+
+const ordercheck = document.querySelector(".pay-button");
+
+ordercheck.addEventListener('click', function(event){
+
+    var modalPop1 = $('.modal-wrap1');
+    var modalBg1 = $('.modal-bg1');
+
+    $(modalPop1).show();
+    $(modalBg1).show();
+})
+
+const payCredit = document.querySelector(".modal-close");
+
+payCredit.addEventListener('click', function(event){
+    var modalPop1 = $('.modal-wrap1');
+    var modalBg1 = $('.modal-bg1');
+
+    var modalPop1 = $('.modal-wrap2');
+    var modalBg1 = $('.modal-bg2');
+
+    $(modalPop1).hide();
+    $(modalBg1).hide();
+
+    $(modalPop2).show();
+    $(modalBg2).show();
+
+})
+
+const adminLoginBtn = document.querySelector(".admin-login-btn");
+
+adminLoginBtn.onclick = () => {
+    location.href="/account/register";
+}
+
