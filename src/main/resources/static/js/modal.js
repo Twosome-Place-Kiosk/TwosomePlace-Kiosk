29b28dd9df9955d8148d1c8ebe482b8477037f88
Lee Jugeon0129 <4216ljg@naver.com>
2022-11-24 18:46:15 +0900

    브랜치 되돌리기 작업중

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -15,21 +15,3 @@ function popClose() {
    $(modalBg).hide();
 
 }
-
-function popOpen1() {
-    var modalPop1 = $('.modal-wrap1');
-    var modalBg1 = $('.modal-bg1');
-
-    $(modalPop1).show();
-    $(modalBg1).show();
-
-}
-
-function popClose1() {
-   var modalPop1 = $('.modal-wrap1');
-   var modalBg1 = $('.modal-bg1');
-
-   $(modalPop1).hide();
-   $(modalBg1).hide();
-
-}
--- src/main/resources/static/js/slideShow.js
+++ src/main/resources/static/js/slideShow.js
@@ -1,9 +1,9 @@
 const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
 const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
 
-let currentIdx = 0; //현재 슬라이드 index
+let currentIdx = 0; //현재 슬라이드 inde
 
-const slideCount = slideImg.length; // 슬라이드 개수 12
+const slideCount = slideImg.length % 5 == 0 ? slideImg.length / 5 : Math.floor(slideImg.length / 5) + 1; // 슬라이드 개수 12
 const prev = document.querySelector('.moving-left'); //이전 버튼
 const next = document.querySelector('.moving-right'); //다음 버튼
 const slideWidth = 600; //한개의 슬라이드 넓이
@@ -11,6 +11,8 @@ const slideMargin = 5; //슬라이드간의 margin 값
 
 //전체 슬라이드 컨테이너 넓이 설정
 slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
+
+
 
 function moveSlide(num) {
   slides.style.left = -num * 400 + 'px';
@@ -23,6 +25,13 @@ prev.addEventListener('click', function () {
   currentIdx !==0일때만 moveSlide 함수 불러옴 */
 
   if (currentIdx !== 0) moveSlide(currentIdx - 1);
+  console.log(
+    `
+    slideCount: ${slideCount}
+    currentIdx: ${currentIdx}
+    `
+    
+    );
 });
 
 next.addEventListener('click', function () {
@@ -30,7 +39,14 @@ next.addEventListener('click', function () {
   다음 버튼 눌러도 아무런 반응 없게 하기 위해
   currentIdx !==slideCount - 1 일때만 
   moveSlide 함수 불러옴 */
-  if (currentIdx !== slideCount - 1) {
+  if (currentIdx < slideCount) {
     moveSlide(currentIdx + 1);
   }
+  console.log(
+    `
+    slideCount: ${slideCount}
+    currentIdx: ${currentIdx}
+    `
+    
+    );
 });
