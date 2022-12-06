dd966128b71f38bde375fed14fd6731b0bfe3e3b
YoungWoong22 <hwa8078678@naver.com>
2022-12-06 16:41:43 +0900

    수정버튼 성공

--- src/main/java/com/twosomekiosk/twosome202211114/service/admin/ProductManagementServiceImpl.java
+++ src/main/java/com/twosomekiosk/twosome202211114/service/admin/ProductManagementServiceImpl.java
@@ -119,69 +119,15 @@ public class ProductManagementServiceImpl implements ProductManagementService {
 
     @Override
     public void updateRegister(ProductRegisterRespDto productRegisterRespDto) throws Exception {
-        if(productRegisterRespDto.getFiles() == null) {
-            Map<String, String> errorMap = new HashMap<String, String>();
-            errorMap.put("error", "이미지를 선택하지 않았습니다.");
-            throw new CustomValidationException("Img Error", errorMap);
-        }
 
-        List<Product> products = new ArrayList<Product>();
+        Product product = (Product.builder()
+                .id(productRegisterRespDto.getId())
+                .category_id(productRegisterRespDto.getCategory())
+                .pdt_name(productRegisterRespDto.getName())
+                .pdt_price(productRegisterRespDto.getPrice())
+                .build());
 
-        productRegisterRespDto.getFiles().forEach(file -> {
-            Resource resource = resourceLoader.getResource("classpath:/static/upload/product");
-            String targetFilePath  = null;
-            String srcFilePath = null;
-
-            System.out.println(productRegisterRespDto.getFiles());
-            try{
-                //해당경로에 이폴더가 존재하냐
-                if(!resource.exists()) {
-                    String targetTempPath = resourceLoader.getResource("classpath:/static").getURI().toString();
-                    String srcTempPath = resourceLoader.getResource("classpath:/static").getURI().toString();
-                    targetTempPath = "/" + targetTempPath.substring(targetTempPath.indexOf("/") + 1);
-                    srcTempPath = "/" + srcTempPath.substring(srcTempPath.indexOf("/") + 1, srcTempPath.indexOf("target")) + "/src/main/resources/static";
-
-                    System.out.println(targetTempPath);
-                    System.out.println(srcTempPath);
-                    File f = new File(targetTempPath + "/upload/product");
-                    f.mkdirs();
-                    f = new File(srcTempPath + "/upload/product");
-                    f.mkdirs();
-                }
-
-                targetFilePath = "/" + resource.getURI().toString().substring(resource.getURI().toString().indexOf("/") + 1);
-                srcFilePath = "/" + resource.getURI().toString().substring(resource.getURI().toString().indexOf("/") + 1, resource.getURI().toString().indexOf("target")) + "/src/main/resources/static/upload/product";
-                System.out.println(targetFilePath);
-                System.out.println(srcFilePath);
-
-            } catch (IOException e){
-                throw new RuntimeException(e);
-            }
-
-            String originName = file.getOriginalFilename();
-            String extension = originName.substring(originName.lastIndexOf("."));
-            String saveName = UUID.randomUUID().toString().replace("-","") + extension;
-            Path targetPath = Paths.get(targetFilePath + "/" + saveName);
-            Path srcPath = Paths.get(srcFilePath + "/" + saveName);
-
-            try {
-                Files.write(targetPath, file.getBytes());
-                Files.write(srcPath, file.getBytes());
-
-            } catch (IOException e) {
-                throw new CustomInternalServerErrorException(e.getMessage());
-            }
-
-            products.add(Product.builder()
-                    .id(productRegisterRespDto.getId())
-                    .category_id(productRegisterRespDto.getCategory())
-                    .pdt_name(productRegisterRespDto.getName())
-                    .pdt_price(productRegisterRespDto.getPrice())
-                    .origin_name(originName)
-                    .build());
-        });
-        productManagementRepository.updateProduct(products);
-
+        productManagementRepository.updateProduct(product);
     }
 
     @Override
